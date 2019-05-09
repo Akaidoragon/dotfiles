0f6cd1dd05057b63cf97c417a86942be4b22730e
Le Anh Duc <anhdle14@hotmail.com>
2019-05-09 14:12:01 +0900

    Thu May  9 14:12:01 JST 2019

--- .bin/dotfiles-replication.sh
+++ .bin/dotfiles-replication.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 git clone --separate-git-dir=$HOME/.dotfiles https://github.com/anhdle14/dotfiles.git $HOME/.dotfiles.bak
+alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 rsync --recursive --verbose --exclude '.git' $HOME/.dotfiles.bak/ $HOME/
-rm --recursive $HOME/.dotfiles.bak
+rm -rf $HOME/.dotfiles.bak
+
+## Symlinks
+### Tmux
+ln -s $HOME/Developer/git/.tmux/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/Developer/git/.tmux/.tmux.conf.local $HOME/.tmux.conf.local
+
+### Nvim & Vim
+ln -s $HOME/.config/nvim/init.vim $HOME/.vimrc
+
+### ZSH
+ln -s $HOME/Developer/git/grml-etc-core/etc/zsh/zlogin $HOME/.zlogin
+ln -s $HOME/Developer/git/grml-etc-core/etc/zsh/zlogout $HOME/.zlogout
+ln -s $HOME/Developer/git/grml-etc-core/etc/zsh/zprofile $HOME/.zprofile
+ln -s $HOME/Developer/git/grml-etc-core/etc/zsh/zshenv $HOME/.zshenv
+ln -s $HOME/Developer/git/grml-etc-core/etc/zsh/zshrc $HOME/.zshrc
+
--- .bin/dotfiles-setup.sh
+++ .bin/dotfiles-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 alias config='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 git init --bare $HOME/.dotfiles
-config remote add origin git@github.com:anhdle14/dotfiles.git
 config config status.showUntrackedFiles no
+config remote add origin git@github.com:anhdle14/dotfiles.git
 config remote set-url origin git@github.com:anhdle14/dotfiles.git
